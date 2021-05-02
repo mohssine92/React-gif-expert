7b9f05190838b65c7ee8512a604595a6390f82f0
mohssine92 <mohssinelmariouh@gmail.com>
2021-05-02 13:23:28 +0200

    69 - GifExpertApp - Component Function Recat

---
+++ src/GifExpertApp.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+
+/* Function component */
+const GifExpertApp = () => {
+
+
+   return(
+     <>
+        <h2>GifExpertApp</h2>
+        <hr />
+        
+       
+     </>
+
+   );
+
+}
+
+
+/* CounterApp.propTypes = {
+  value: PropTypes.number
+  
+} */
+
+/* PrimeraApp.defaultProps = {
+  subtitulo:'Soy pokemon por defecto'
+
+} */
+
+
+
+export default GifExpertApp;
