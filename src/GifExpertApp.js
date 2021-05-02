3739e996903847f689ab934a1c4f03c0b243165b
mohssine92 <mohssinelmariouh@gmail.com>
2021-05-02 14:44:18 +0200

    70 - Creando una lista de categorias , hook de useState , key irrepetible

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 /* Function component */
 const GifExpertApp = () => {
 
+  /*const categories = ['One Puch','Samurai X', 'Dragon Ball'];  */ 
+
+  /* Hooke useState :Forma  para decirle a React tuvo cambio , vuelva a renderizar componente ,  */
+  const [categories, setCategories] = useState(['One Puch','Samurai X', 'Dragon Ball']);
+
+  const handleAd = () => {
+   /*  setCategories([...categories, 'Goku']);*/
+   setCategories( cats =>[...cats, 'Goku']);
+  }
+
+
 
    return(
      <>
         <h2>GifExpertApp</h2>
         <hr />
-        
+        <button onClick={ handleAd }>Agregar</button>
+        <ol>
+          {
+            categories.map( category  => {
+               return <li key={ category }>{ category }</li>
+            })
+         }
+        </ol>   
        
      </>
 
