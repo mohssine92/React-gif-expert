c86dd463ad2b6e9e63ea42f35e521b4f69e2ae59
mohssine92 <mohssinelmariouh@gmail.com>
2021-05-02 12:53:33 +0200

    68 - instalacion de nuevo proyecto de React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
+  
+ 
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
       </header>
     </div>
